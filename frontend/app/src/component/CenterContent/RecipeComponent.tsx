c55794f10cec15f9c95469bed5381bedb911e3b0
nobukunisyota <nobukuni.syota0727@gmail.com>
2023-12-20 22:59:18 +0900

    Add axios package

--- frontend/app/src/component/CenterContent/RecipeComponent.tsx
+++ frontend/app/src/component/CenterContent/RecipeComponent.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect }  from 'react'
+import axios from 'axios';
 import './RecipeComponent.scss'
 
 function RecipeComponent() {
+    const [post, setPosts] = useState([])
+    useEffect(() => {
+        axios.get('')
+        .then(res => {
+            console.log(res.data)
+        })
+    }, [])
     return (
-    <div>RecipeComponent</div>
+    <div></div>
     )
 }
 
