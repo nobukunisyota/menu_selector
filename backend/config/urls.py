7db414d74c5d0dc98b47aa824707e413b48ceb10
nobukunisyota <nobukuni.syota0727@gmail.com>
2023-12-23 15:17:06 +0900

    modify top router

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
+router = routers.SimpleRouter()
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include((router.urls, 'recipes'), namespace='recipes')),
 ]
