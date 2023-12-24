a35214554d3b252a198e99da82356d97e4aecfc6
nobukunisyota <nobukuni.syota0727@gmail.com>
2023-12-24 22:32:56 +0900

    Update Recipe App

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    ingredients = models.TextField()
+    directions = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
