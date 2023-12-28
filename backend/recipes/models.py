0bc4fd11e01458d05b6327db312b28cf246aa61f
nobukunisyota <nobukuni.syota0727@gmail.com>
2023-12-28 23:24:56 +0900

    Add User Custom Model

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    pass
 
 
 class Recipe(models.Model):
+    class Meta:
+        db_table = 'recipes'
+
     name = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
