23d74c1c81b22c2213b68b91919afc23907fa7d9
nobukunisyota <nobukuni.syota0727@gmail.com>
2023-12-30 22:52:41 +0900

    Add CRUD Feature

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -3,7 +3,19 @@ from rest_framework import routers
 from .views import *
 
 router = routers.DefaultRouter()
-router.register('recipes', RecipeViewSet, basename='recipe')
+router.register('recipes', RecipeListViewSet, basename='recipe')
+router.register('recipes', RecipeDetailViewSet, basename='recipe')
+router.register('recipes', RecipeCreateViewSet, basename='recipe')
+router.register('recipes', RecipeUpdateViewSet, basename='recipe')
+router.register('recipes', RecipeDeleteViewSet, basename='recipe')
 urlpatterns = [
-    path('recipes/', include(router.urls)),
+    path('recipes/', RecipeListViewSet.as_view({'get': 'list'})),
+    path('recipes/<int:pk>/',
+         RecipeDetailViewSet.as_view({'get': 'retrieve'})),
+    path('recipes/add/', RecipeCreateViewSet.as_view({'post': 'create'})),
+    path('recipes/update/<int:pk>/',
+         RecipeUpdateViewSet.as_view({'put': 'update'})),
+    path('recipes/delete/<int:pk>/',
+         RecipeDeleteViewSet.as_view({'delete': 'destroy'})),
+
 ]
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from .models import *
 from .serializer import *
 
 
-class RecipeViewSet(viewsets.ModelViewSet):
+class RecipeListViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class RecipeDetailViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class RecipeCreateViewSet(mixins.CreateModelMixin,
+                          mixins.ListModelMixin,
+                          mixins.RetrieveModelMixin,
+                          viewsets.GenericViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class RecipeUpdateViewSet(mixins.UpdateModelMixin,
+                          mixins.ListModelMixin,
+                          mixins.RetrieveModelMixin,
+                          viewsets.GenericViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class RecipeDeleteViewSet(mixins.DestroyModelMixin,
+                          mixins.ListModelMixin,
+                          mixins.RetrieveModelMixin,
+                          viewsets.GenericViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
